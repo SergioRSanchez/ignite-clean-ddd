5fbd1b550a7616197bd88de9a458084c486bc509
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-14 19:44:17 -0300

    feat: added getters and setters to entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -11,8 +11,45 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  // GETTER para retornar o valor da propriedade authorId
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
   get content() {
     return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  // GETTER para retornar o valor da propriedade content e limita-o a 120 caracteres
+  get excerpt() {
+    return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  // Método privado para alterar o valor da propriedade updatedAt
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  // SETTER para alterar o valor da propriedade content e chamar o método que atualiza o valor da propriedade updatedAt
+  set content(content: string) {
+    this.props.content = content
+
+    this.touch()
   }
 
   static create(
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs'
+
 import { Slug } from './value-objects/slug'
 import { Entity } from '../../core/entities/entity'
 import { UniqueEntityID } from '../../core/entities/unique-entity-id'
@@ -14,12 +16,79 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  // GETTER que verifica se a pergunta foi criada ha menos de 3 dias
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  // GETTER para retornar o valor da propriedade content e limita-o a 120 caracteres
+  get excerpt() {
+    return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  // Método privado para alterar o valor da propriedade updatedAt
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  // SETTER para alterar o valor da propriedade title e chamar o método que atualiza o valor da propriedade updatedAt
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+
+    this.touch()
+  }
+
   static create(
-    props: Optional<QuestionProps, 'createdAt'>,
+    props: Optional<QuestionProps, 'slug' | 'createdAt'>,
     id?: UniqueEntityID
   ) {
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
       createdAt: new Date(),
     }, id)
 
