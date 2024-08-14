7fa2dbadea5e34daf5c7db7093eeb65837b3be17
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-14 16:15:16 -0300

    feat: value objects from slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest';
+
+import { Slug } from './slug';
+
+test('should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Título -de_exemplo/')
+
+  expect(slug.value).toEqual('titulo-de-exemplo')
+})
