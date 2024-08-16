ad3992036693ca0b0f40f395bc2ccbd97e8b67f2
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-16 08:36:55 -0300

    refactor: refactored folder structure for the Clean Architecture principle

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,9 +1,10 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { Answer } from '../entities/answer'
+import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {},
+  // eslint-disable-next-line prettier/prettier
+  create: async (answer: Answer) => { },
 }
 
 test('create answer', async () => {
