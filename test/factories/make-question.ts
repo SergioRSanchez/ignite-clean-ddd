2d157c47ba9982bec533df1fbfca5eeb8a5bf0d5
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-19 11:20:37 -0300

    feat: generating fake data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
@@ -5,14 +7,19 @@ import {
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID('1'),
-    title: 'Example Question',
-    content: 'Example Content',
-    slug: Slug.create('example-question'),
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
