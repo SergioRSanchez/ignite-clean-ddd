f102d4c9778f6a98f6e95f01a548467f9985edbc
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-19 11:06:50 -0300

    feat: created question factory

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,13 +1,17 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { Question } from '@/domain/forum/enterprise/entities/question'
+import {
+  Question,
+  QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion() {
+export function makeQuestion(override: Partial<QuestionProps> = {}) {
   const question = Question.create({
     authorId: new UniqueEntityID('1'),
     title: 'Example Question',
     content: 'Example Content',
     slug: Slug.create('example-question'),
+    ...override,
   })
 
   return question
