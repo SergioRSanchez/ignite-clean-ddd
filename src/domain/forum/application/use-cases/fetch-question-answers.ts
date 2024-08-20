7bc650fcac4c14aef5b4e8d443b14ed703b36935
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-20 16:12:54 -0300

    feat: implement use case for fetch question answers

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,30 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionAnswersUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private answersRepository: AnswersRepository) { }
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page },
+    )
+
+    return {
+      answers,
+    }
+  }
+}
