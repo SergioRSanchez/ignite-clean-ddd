420104e8d2f44f2fd937be39013a62b2b26f29c2
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-20 18:15:21 -0300

    feat: created base class comment and extend it in answer and question comment

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,43 +1,14 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityID
+export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
-  // GETTER para retornar o valor da propriedade authorId
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  // Método privado para alterar o valor da propriedade updatedAt
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  // SETTER para alterar o valor da propriedade content e chamar o método que atualiza o valor da propriedade updatedAt
-  set content(content: string) {
-    this.props.content = content
-
-    this.touch()
+export class AnswerComment extends Comment<AnswerCommentProps> {
+  get answerId() {
+    return this.props.answerId
   }
 
   static create(
