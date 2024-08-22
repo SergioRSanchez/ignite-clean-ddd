59bddcee90c1a929077037fc774f9495c8d6e935
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-22 18:41:36 -0300

    feat: implement user case to delete answer comment

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -0,0 +1,36 @@
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+interface DeleteAnswerCommentUseCaseRequest {
+  authorId: string
+  answerCommentId: string
+}
+
+// eslint-disable-next-line prettier/prettier
+interface DeleteAnswerCommentUseCaseResponse { }
+
+export class DeleteAnswerCommentUseCase {
+  constructor(
+    private answerCommentsRepository: AnswerCommentsRepository,
+    // eslint-disable-next-line prettier/prettier
+  ) { }
+
+  async execute({
+    authorId,
+    answerCommentId,
+  }: DeleteAnswerCommentUseCaseRequest): Promise<DeleteAnswerCommentUseCaseResponse> {
+    const answerComment =
+      await this.answerCommentsRepository.findById(answerCommentId)
+
+    if (!answerComment) {
+      throw new Error('Answer comment not found.')
+    }
+
+    if (answerComment.authorId.toString() !== authorId) {
+      throw new Error('Not allowed.')
+    }
+
+    await this.answerCommentsRepository.delete(answerComment)
+
+    return {}
+  }
+}
